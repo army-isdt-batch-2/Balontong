17c5e77e48f33aee143e6c4c968f257929ed7a0c
balontongre <romar.balontong1995@gmail.com>
2021-05-26 16:42:27 +0800

    payroll codes

--- database/migrations/2021_05_24_070229_create_departments_table.php
+++ database/migrations/2021_05_24_070229_create_departments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDepartrmentsTable extends Migration
+class CreateDepartmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,9 @@ class CreateDepartrmentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('department', function (Blueprint $table) {
+        Schema::create('departments', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('middle_name');
-            $table->integer('age');
-            $table->enum('gender', [
-                'Male', 'Female'
-            ]);
-            $table->date('birthday');
-            $table->integer('contact');
-            $table->longText('address');
+            $table->string('departments');
 
             $table->softDeletes();
             $table->timestamps();
@@ -38,6 +29,6 @@ class CreateDepartrmentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('department');
+        Schema::dropIfExists('departments');
     }
 }
--- resources/views/create_form/departments.blade.php
+++ resources/views/create_form/departments.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Department Personnel</title>
+    <title>Create Personnel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
@@ -15,7 +15,7 @@
     <div class="container">
         <div class="row p-5">
             <div class="col-10">
-                <h1>Create Department Personels</h1>
+                <h1>Create Departments</h1>
             </div>
             <div class="col-2">
                 <a href="/" class="btn btn-danger">Cancel</a>
@@ -24,42 +24,12 @@
                 <form action="/create/save" method="post">
                     @csrf
                     <div class="mb-3">
-                        <label class="form-label">First Name</label>
-                        <input type="text" class="form-control" name="first_name" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Last Name</label>
-                        <input type="text" class="form-control" name="last_name" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Middle Name</label>
-                        <input type="text" class="form-control" name="middle_name" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Age</label>
-                        <input type="number" class="form-control" name="age" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Contact</label>
-                        <input type="number" class="form-control" name="contact" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Birthday</label>
-                        <input type="date" class="form-control" name="birthday" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Gender</label>
-                        <select class="form-control" name="gender" required>
-                            <option>Male</option>
-                            <option>Female</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Address</label>
-                        <textarea class="form-control" name="address" rows="3"></textarea>
-                    </div>
-                    <div class="mb-3">
-                        <button class="btn btn-success">Save</button>
+                        <label class="form-label">Department</label>
+                        <input type="text" class="form-control" name="departments" required>
+                        <br><br>
+                        <div class="mb-3">
+                          <button class="btn btn-success">Save</button>
+                      </div>
                     </div>
                 </form>
             </div>
